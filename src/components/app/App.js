30257250a4f636489247f5a44e148a2193826495
Anvar <my@email.com>
2023-11-19 01:47:01 +0300

    Connected routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,8 @@ import blueBlur from '../../resourses/Blue.svg'
 
 import './App.scss';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
 import Container from '../container/Container';
 import Header from '../header/Header';
 import AboutMe from '../aboutMe/AboutMe';
@@ -17,28 +19,37 @@ import MainScreenEntralis from '../mainScreenEntralis/MainScreenEntralis';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      {/* <Container>
-        <div className="App__wrapper">
-          <AboutMe/>
-          <SnakeGame/>
-        </div>
-        <img className='App__blur' src={greenBlur} alt="" />
-        <img className='App__blur' src={blueBlur} alt="" />
-      </Container> */}
-      <div className="App__two-page-wrapper">
-        <Gpspanel>
-          <AsideEntrails/>
-        </Gpspanel>
-        <MainScreen>
-          <Window/>
-          <MainScreenEntralis/>
-          <Scroll/>
-        </MainScreen>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path='/'>
+            <Container>
+              <div className="App__wrapper">
+                <AboutMe/>
+                <SnakeGame/>
+              </div>
+              <img className='App__blur' src={greenBlur} alt="" />
+              <img className='App__blur' src={blueBlur} alt="" />
+            </Container>
+          </Route>
+          <Route exact path='/about'>
+            <main className="App__two-page-wrapper">
+              <Gpspanel>
+                <AsideEntrails/>
+              </Gpspanel>
+              <MainScreen>
+                <Window/>
+                <MainScreenEntralis/>
+                <Scroll/>
+              </MainScreen>
+            </main>
+          </Route>
+        </Switch>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>
+    </Router>
+    
   );
 }
 
